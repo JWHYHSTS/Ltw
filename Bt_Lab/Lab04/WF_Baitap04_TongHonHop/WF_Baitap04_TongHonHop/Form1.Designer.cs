f17e2c893072e30365bd2ae225770417d4d16326
JWHYHSTS <haidangblck@gmail.com>
2025-09-16 20:40:14 +0700

    bt_lab04

---
+++ Bt_Lab/Lab04/WF_Baitap04_TongHonHop/WF_Baitap04_TongHonHop/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WF_Baitap04_TongHonHop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtn = new TextBox();
+            txtx = new TextBox();
+            txts1 = new TextBox();
+            txts2 = new TextBox();
+            txts3 = new TextBox();
+            txts4 = new TextBox();
+            btntinh = new Button();
+            btnnhaplai = new Button();
+            btnexit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(192, 255, 255);
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label1.Location = new Point(354, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(203, 28);
+            label1.TabIndex = 0;
+            label1.Text = "TỔNG HỖN HỢP";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label2.Location = new Point(101, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 26);
+            label2.TabIndex = 1;
+            label2.Text = "Số Mũ N";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label3.Location = new Point(101, 167);
+            label3.Name = "label3";
+            label3.Size = new Size(103, 26);
+            label3.TabIndex = 2;
+            label3.Text = "Giá trị X";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label4.Location = new Point(101, 308);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 26);
+            label4.TabIndex = 3;
+            label4.Text = "S1 =";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label5.Location = new Point(101, 379);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 26);
+            label5.TabIndex = 4;
+            label5.Text = "S2 =";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label6.Location = new Point(101, 441);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 26);
+            label6.TabIndex = 5;
+            label6.Text = "S3 =";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label7.Location = new Point(101, 507);
+            label7.Name = "label7";
+            label7.Size = new Size(57, 26);
+            label7.TabIndex = 6;
+            label7.Text = "S4 =";
+            // 
+            // txtn
+            // 
+            txtn.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            txtn.Location = new Point(216, 104);
+            txtn.Name = "txtn";
+            txtn.Size = new Size(200, 35);
+            txtn.TabIndex = 7;
+            // 
+            // txtx
+            // 
+            txtx.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            txtx.Location = new Point(216, 166);
+            txtx.Name = "txtx";
+            txtx.Size = new Size(200, 35);
+            txtx.TabIndex = 8;
+            // 
+            // txts1
+            // 
+            txts1.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            txts1.Location = new Point(216, 305);
+            txts1.Name = "txts1";
+            txts1.Size = new Size(556, 35);
+            txts1.TabIndex = 9;
+            // 
+            // txts2
+            // 
+            txts2.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            txts2.Location = new Point(216, 376);
+            txts2.Name = "txts2";
+            txts2.Size = new Size(556, 35);
+            txts2.TabIndex = 10;
+            // 
+            // txts3
+            // 
+            txts3.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            txts3.Location = new Point(216, 438);
+            txts3.Name = "txts3";
+            txts3.Size = new Size(556, 35);
+            txts3.TabIndex = 11;
+            // 
+            // txts4
+            // 
+            txts4.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            txts4.Location = new Point(216, 504);
+            txts4.Name = "txts4";
+            txts4.Size = new Size(556, 35);
+            txts4.TabIndex = 12;
+            // 
+            // btntinh
+            // 
+            btntinh.BackColor = Color.Lime;
+            btntinh.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            btntinh.Location = new Point(645, 100);
+            btntinh.Name = "btntinh";
+            btntinh.Size = new Size(150, 46);
+            btntinh.TabIndex = 13;
+            btntinh.Text = "Tính";
+            btntinh.UseVisualStyleBackColor = false;
+            btntinh.Click += btntinh_Click;
+            // 
+            // btnnhaplai
+            // 
+            btnnhaplai.BackColor = Color.Cyan;
+            btnnhaplai.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            btnnhaplai.Location = new Point(645, 167);
+            btnnhaplai.Name = "btnnhaplai";
+            btnnhaplai.Size = new Size(150, 46);
+            btnnhaplai.TabIndex = 14;
+            btnnhaplai.Text = "Nhập lại";
+            btnnhaplai.UseVisualStyleBackColor = false;
+            btnnhaplai.Click += btnnhaplai_Click;
+            // 
+            // btnexit
+            // 
+            btnexit.BackColor = Color.Red;
+            btnexit.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            btnexit.Location = new Point(778, 405);
+            btnexit.Name = "btnexit";
+            btnexit.Size = new Size(150, 46);
+            btnexit.TabIndex = 15;
+            btnexit.Text = "EXIT";
+            btnexit.UseVisualStyleBackColor = false;
+            btnexit.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(951, 586);
+            Controls.Add(btnexit);
+            Controls.Add(btnnhaplai);
+            Controls.Add(btntinh);
+            Controls.Add(txts4);
+            Controls.Add(txts3);
+            Controls.Add(txts2);
+            Controls.Add(txts1);
+            Controls.Add(txtx);
+            Controls.Add(txtn);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtn;
+        private TextBox txtx;
+        private TextBox txts1;
+        private TextBox txts2;
+        private TextBox txts3;
+        private TextBox txts4;
+        private Button btntinh;
+        private Button btnnhaplai;
+        private Button btnexit;
+    }
+}
---
+++ Bt_Lab/Lab04/WF_Baitap04_TongHonHop/WF_Baitap04_TongHonHop/Form1.cs
@@ -0,0 +1,72 @@
+﻿namespace WF_Baitap04_TongHonHop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Bạn có chắc muốn thoát không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                txtn.Focus();
+            }
+        }
+
+        private void btntinh_Click(object sender, EventArgs e)
+        {
+            int N;
+            double X;
+            if (!int.TryParse(txtn.Text, out N) || (N <= 0))
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên cho N.", "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!double.TryParse(txtx.Text, out X))
+            {
+                MessageBox.Show("Vui lòng nhập số thực cho X.", "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            double S4 = 0;
+            string S1 = string.Empty;
+            string S2 = string.Empty;
+            string S3 = string.Empty;
+            for (int i = 1; i <= N; i++)
+            {
+                S1 += $"X" + (i > 1 ? $"^{i}" : "") + "+";
+                S2 += $"{X}{(i > 1 ? i.ToString() : "")}+";
+                S3 += $"{Math.Pow(X, i)}+";
+                S4 += Math.Pow(X, i);
+            }
+            txts1.Text = S1.Substring(0, S1.Length - 1);
+            txts2.Text = S2.Substring(0, S2.Length - 1);
+            txts3.Text = S3.Substring(0, S3.Length - 1);
+            txts4.Text = S4.ToString();
+        }
+
+        private void btnnhaplai_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Bạn có chắc muốn nhập lại không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                txtn.Clear();
+                txtx.Clear();
+                txts1.Clear();
+                txts2.Clear();
+                txts3.Clear();
+                txts4.Clear();
+                txtn.Focus();
+            }
+        }
+    }
+}
